9629f4b577504b532e1918e525593d0211d260ce
Sanket Raghunath Bobhate <76879335+SankeT2705@users.noreply.github.com>
2022-12-18 19:11:36 +0530

    Updates

---
+++ precache-manifest.dfe0759dc1ca1a8a319b99c37d8ef0fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3a21c74f09b15ee3d67",
+    "url": "/static/js/main.e3a21c74.chunk.js"
+  },
+  {
+    "revision": "ac8727d7eac645afad3f",
+    "url": "/static/js/1.ac8727d7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "058c44518e8424227a0a7bc418aed269",
+    "url": "/index.html"
+  }
+];
